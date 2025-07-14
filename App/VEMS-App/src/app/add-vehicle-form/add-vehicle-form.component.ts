ea899b0cbbbadc5cbe7b095954b6f9a0250a1396
DarshanGA <darshanjadav2016@gmail.com>
2025-07-15 01:18:21 +0530

    added 'Add Vehicle' form navigation, its basic fields till specification section

--- App/VEMS-App/src/app/add-vehicle-form/add-vehicle-form.component.ts
+++ App/VEMS-App/src/app/add-vehicle-form/add-vehicle-form.component.ts
@@ -1,19 +1,39 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { fuelTypeDropdownOptions, vehicleTypeDropdownOptions } from '../../../temp_data/fuelTypeDropdown.data';
 
 @Component({
   selector: 'app-add-vehicle-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-vehicle-form.component.html',
   styleUrl: './add-vehicle-form.component.css'
 })
 export class AddVehicleFormComponent {
 
-  @Input() shouldShowForm = false;
   @Output() formClosedEvent = new EventEmitter<void>();
+
+  form!: FormGroup;
+  addVehicleFormTitle = "Add a Vehicle";
+  vehicleTypeMandatorWarning = "Vehicle Type selection is mandatory";
+  fuelTypeMandatoryWarning = "Fuel Type selection is mandatory";
+  vehicleTypesOption = vehicleTypeDropdownOptions;
+  fuelTypeOptions = fuelTypeDropdownOptions;
+
+  constructor(private fb: FormBuilder){  }
+
+  ngOnInit(): void{
+
+    this.form = this.fb.group({
+
+      vehicleType: ['', Validators.required],
+      fuelType: ['', Validators.required],
+      vehicleName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+      vehicleDescription: ['', Validators.maxLength(255)]
+    });
+  }
 
   closeForm(){
 
-    this.shouldShowForm = false;
     this.formClosedEvent.emit();
   }
 }
---
+++ App/VEMS-App/temp_data/fuelTypeDropdown.data.ts
@@ -0,0 +1,3 @@
+export const vehicleTypeDropdownOptions = ["Bike", "Car", "Pickup", "Truck", "Tractor", "Excavator", "Others"];
+
+export const  fuelTypeDropdownOptions = ["Diesel", "EV", "LNG", "Petrol"];
