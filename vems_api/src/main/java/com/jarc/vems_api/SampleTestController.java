47b560d0d607e0339b4a0f92e745b354ecc10975
DarshanGA <darshanjadav2016@gmail.com>
2025-07-17 22:47:48 +0530

    Remaining required files for the API spring boot module.

--- vems_api/src/main/java/com/jarc/vems_api/SampleTestController.java
+++ vems_api/src/main/java/com/jarc/vems_api/SampleTestController.java
@@ -1,2 +1,14 @@
-package com.jarc.vems_api;public class SampleTestController {
+package com.jarc.vems_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleTestController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+
+        return "Hello World!";
+    }
 }
